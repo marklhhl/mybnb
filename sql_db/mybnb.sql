7f11c00b903bd69ac2fb3704b04208269de1380a
Jun Chao Chen <32042228+Junjun1216@users.noreply.github.com>
2019-07-21 04:08:38 -0400

    added mybnb schema/triggers and populate_db script

---
+++ sql_db/mybnb.sql
@@ -0,0 +1,342 @@
+-- MySQL dump 10.13  Distrib 5.7.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mybnb
+-- ------------------------------------------------------
+-- Server version	5.7.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `avaliability`
+--
+
+DROP TABLE IF EXISTS `avaliability`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `avaliability` (
+  `Lid` int(11) NOT NULL,
+  `Caid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `calendar`
+--
+
+DROP TABLE IF EXISTS `calendar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `calendar` (
+  `Caid` int(11) NOT NULL AUTO_INCREMENT,
+  `avaliable_from` date NOT NULL,
+  `avaliable_till` date NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`Caid`)
+) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cmade_to_list`
+--
+
+DROP TABLE IF EXISTS `cmade_to_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cmade_to_list` (
+  `to_list` int(11) NOT NULL,
+  `from_user` int(11) NOT NULL,
+  `Cid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cmade_to_user`
+--
+
+DROP TABLE IF EXISTS `cmade_to_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cmade_to_user` (
+  `to_user` int(11) NOT NULL,
+  `from_user` int(11) NOT NULL,
+  `Cid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `Cid` int(11) NOT NULL AUTO_INCREMENT,
+  `rating` decimal(2,1) NOT NULL,
+  `date` date NOT NULL,
+  `comment` varchar(500) NOT NULL,
+  `comment_type` char(4) NOT NULL,
+  PRIMARY KEY (`Cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `mybnb`.`comment_BEFORE_INSERT` BEFORE INSERT ON `comment` FOR EACH ROW
+BEGIN
+declare msg varchar(255);
+if new.rating < 1 or new.rating > 5 then
+set msg = 'Constraint violated: rating >= 1 AND <= 5';
+signal sqlstate '45000' set message_text = msg;
+end if;
+if new.comment_type != 'user' and new.comment_type != 'list' then
+set msg = 'Constraint violated: comment_type must be "list" or "user"';
+signal sqlstate '45000' set message_text = msg;
+end if;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `history`
+--
+
+DROP TABLE IF EXISTS `history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `history` (
+  `Hid` int(11) NOT NULL AUTO_INCREMENT,
+  `start` date NOT NULL,
+  `end` date NOT NULL,
+  `transaction_date` date NOT NULL,
+  `cost_per_day` decimal(10,2) NOT NULL,
+  `total_cost` decimal(10,2) NOT NULL,
+  `status` varchar(9) NOT NULL,
+  PRIMARY KEY (`Hid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3334 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `mybnb`.`history_BEFORE_INSERT` BEFORE INSERT ON `history` FOR EACH ROW
+BEGIN
+declare msg varchar(255);
+if new.status != 'Pending' AND new.status != 'Completed' AND new.status != 'Canceled' then
+set msg = 'Constraint violated: status must be "Pending" or "Completed" or "Canceled"';
+signal sqlstate '45000' set message_text = msg;
+end if;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `host`
+--
+
+DROP TABLE IF EXISTS `host`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `host` (
+  `Uid` int(11) NOT NULL AUTO_INCREMENT,
+  `addr` varchar(255) NOT NULL,
+  `countrey` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `postal_code` char(6) NOT NULL,
+  `birthday` date NOT NULL,
+  `sin` int(9) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `occupation` varchar(100) NOT NULL,
+  PRIMARY KEY (`Uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `mybnb`.`host_BEFORE_INSERT` BEFORE INSERT ON `host` FOR EACH ROW
+BEGIN
+declare msg varchar(255);
+    IF NEW.birthday > str_to_date('July 21 2001', '%M %d %Y')  then
+		set msg ='Constraint Violated: age must be 18 or older';
+       signal sqlstate '45000' set message_text = msg;
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `listing`
+--
+
+DROP TABLE IF EXISTS `listing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `listing` (
+  `Lid` int(11) NOT NULL AUTO_INCREMENT,
+  `home_type` varchar(200) NOT NULL,
+  `longtitude` decimal(9,3) NOT NULL,
+  `latitude` decimal(9,3) NOT NULL,
+  `city` varchar(200) NOT NULL,
+  `addr` varchar(200) NOT NULL,
+  `postal_code` char(6) NOT NULL,
+  `countrey` varchar(200) NOT NULL,
+  `wifi` varchar(3) NOT NULL,
+  `beds` int(11) NOT NULL,
+  `bathrooms` int(11) NOT NULL,
+  `kitchens` int(11) NOT NULL,
+  `parking` int(11) NOT NULL,
+  `other_accom` varchar(255) DEFAULT NULL,
+  `addtional_comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Lid`),
+  UNIQUE KEY `Lid_UNIQUE` (`Lid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `mybnb`.`listing_BEFORE_INSERT` BEFORE INSERT ON `listing` FOR EACH ROW
+BEGIN
+declare msg varchar(255);
+if new.wifi != 'YES' AND new.wifi != 'NO' then
+set msg = 'Constraint violated: wifi must be "YES" or "NO"';
+signal sqlstate '45000' set message_text = msg;
+end if;
+if new.longtitude < 0 or new.longtitude > 180 or new.latitude < 0 or new.latitude > 180 then
+set msg = 'Constraint violated: invalid Geo-Coordinates';
+signal sqlstate '45000' set message_text = msg;
+end if;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `owned_by`
+--
+
+DROP TABLE IF EXISTS `owned_by`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `owned_by` (
+  `Uid` int(11) NOT NULL,
+  `Lid` int(11) NOT NULL,
+  KEY `owner_idx` (`Uid`),
+  KEY `listing id_idx` (`Lid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `rental_history`
+--
+
+DROP TABLE IF EXISTS `rental_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rental_history` (
+  `renterId` int(11) NOT NULL,
+  `hostId` int(11) NOT NULL,
+  `historyId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `renter`
+--
+
+DROP TABLE IF EXISTS `renter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `renter` (
+  `Uid` int(11) NOT NULL AUTO_INCREMENT,
+  `addr` varchar(255) NOT NULL,
+  `countrey` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `postal_code` char(6) NOT NULL,
+  `birthday` date NOT NULL,
+  `sin` int(9) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `middle_name` varchar(100) DEFAULT NULL,
+  `occupation` varchar(100) NOT NULL,
+  `payment` decimal(16,0) NOT NULL,
+  PRIMARY KEY (`Uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `mybnb`.`renter_BEFORE_INSERT` BEFORE INSERT ON `renter` FOR EACH ROW
+BEGIN
+declare msg varchar(255);
+    IF NEW.birthday > str_to_date('July 21 2001', '%M %d %Y')  then
+		set msg ='Constraint Violated: age must be 18 or older';
+       signal sqlstate '45000' set message_text = msg;
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-07-21  3:53:11
---
+++ sql_db/populate_db.sql
@@ -0,0 +1,87 @@
+delete from host;
+delete from renter;
+delete from listing;
+delete from comment;
+delete from calendar;
+delete from history;
+delete from avaliability;
+delete from owned_by;
+delete from rental_history;
+delete from cmade_to_list;
+delete from cmade_to_user;
+
+insert into host 
+values 
+	(1,'531 george st', 'Canada', 'Toronto', 'M43J21', str_to_date('august 10 1994', '%M %d %Y'), 232133333, 'james', 'charles','eva', 'writer'),
+	(2,'23 mintwood drive', 'Canada', 'Toronto', 'M2M3A6', str_to_date('december 16 1998', '%M %d %Y'), 321542111, 'michael', 'chen',null, 'computer science'),
+	(3,'52 church st', 'Usa', 'New York', 'SF3 4F', str_to_date('august 10 1994', '%M %d %Y'), 653246123, 'john', 'mcnugget',null, 'donut eater'),
+	(4,'1324 st george st', 'Canada', 'Toronto', 'MJ2AF6', str_to_date('september 10 1990', '%M %d %Y'), 644257123, 'Joe', 'Bettrige',null, 'cscb07 torturer'),
+	(5,'923 notaname ko', 'Japan', 'Toyko', 'M431FS', str_to_date('august 10 1970', '%M %d %Y'), 653213612, 'james', 'charles',null, 'salary man');
+
+insert into renter
+values 
+	(10,'531 george st', 'Canada', 'Toronto', 'M43J21', str_to_date('august 10 1994', '%M %d %Y'), 232133333, 'james', 'charles','eva', 'writer', 1555165125523333),
+	(20,'23 mintwood drive', 'Canada', 'Toronto', 'M2M3A6', str_to_date('december 16 1998', '%M %d %Y'), 321542111, 'michael', 'chen',null, 'computer science', 1555165122223333),
+	(30,'52 church st', 'Usa', 'New York', 'SF3 4F', str_to_date('august 10 1994', '%M %d %Y'), 653246123, 'john', 'mcnugget',null, 'donut eater', 9855165122223033),
+	(40,'1324 st george st', 'Canada', 'Toronto', 'MJ2AF6', str_to_date('september 10 1990', '%M %d %Y'), 644257123, 'Joe', 'Bettrige',null, 'cscb07 torturer', 5455165122523333),
+	(50,'923 notaname ko', 'Japan', 'Toyko', 'M431FS', str_to_date('august 10 1970', '%M %d %Y'), 653213612, 'james', 'charles',null, 'salary man', 7655165122223333);
+
+insert into listing
+values
+	(1, 'semi-detach', 123.000000, 132.000000, 'Toronto', '123skymark dr', 'M23M23', 'Canada', 'YES', 4, 1, 1, 1, null, null),
+	(2, 'condo', 13.000000, 32.000000, 'Capte', '23nordivir wy', 'M23M23', 'Iceland', 'YES', 9, 1, 1, 1, null, null),
+	(3, 'cave', 1.000000, 2.000000, 'middleof', 'nowhere', 'AAAAAA', 'nowhereland', 'YES', 2, 1, 1, 1, 'free breakfast', 'why did you come here');
+
+insert into comment
+values
+	(11, 1, str_to_date('may 16 2019', '%M %d %Y'), 'this guy suck', 'user'),
+	(22, 5, str_to_date('may 16 2019', '%M %d %Y'), 'very affordable place, very clean', 'list');
+
+insert into calendar
+values
+	(111, str_to_date('october 16 2019', '%M %d %Y'), str_to_date('october 22 2019', '%M %d %Y'), 300),
+	(222, str_to_date('october 23 2019', '%M %d %Y'), str_to_date('october 30 2019', '%M %d %Y'), 500);
+
+insert into history
+values
+	(1111, str_to_date('september 16 2019', '%M %d %Y'), str_to_date('september 22 2019', '%M %d %Y'), str_to_date('June 1 2019', '%M %d %Y'), 250, 1750, 'Pending'),
+	(2222, str_to_date('June 4 2019', '%M %d %Y'), str_to_date('June 12 2019', '%M %d %Y'), str_to_date('april 1 2019', '%M %d %Y'), 250, 1750, 'Completed'),
+	(3333, str_to_date('august 9 2019', '%M %d %Y'), str_to_date('august 12 2019', '%M %d %Y'), str_to_date('april 1 2019', '%M %d %Y'), 250, 750, 'Canceled');
+
+insert into avaliability
+values
+	(1, 111),
+    (2, 222);
+    
+insert into owned_by
+values
+	(1, 1),
+    (1, 2),
+    (2, 3);
+    
+insert into rental_history
+values
+	(40, 1, 1111),
+    (50, 2, 2222),
+    (30, 5, 3333);
+    
+insert into cmade_to_list
+values
+	(2222, 20, 11);
+    
+insert into cmade_to_user
+values
+	(20, 2, 22);
+
+    
+select * from listing;
+select * from renter;
+select * from host;
+select * from comment;
+select * from calendar;
+select * from history;
+select * from avaliability;
+select * from owned_by;
+select * from rental_history;
+select * from cmade_to_list;
+select * from cmade_to_user;
